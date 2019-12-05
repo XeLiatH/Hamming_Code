5ca19f505d14d7327b778671f0fd35bb549ae2d2
Matěj Beran <matej.beran96@gmail.com>
2019-12-05 20:47:32 +0100

    Add command line parser input handle.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,35 @@
 
 namespace Hamming
 {
+    class Options
+    {
+        [Option('m', "mode", Required = true, HelpText = "k - encode | d - decode")]
+        public char Mode { get; set; }
+
+        [Option('i', "input", Required = true, HelpText = "Input file to be processed.")]
+        public string InputFile { get; set; }
+
+        [Option('o', "output", Required = false, HelpText = "If specified, output is saved to a file.")]
+        public string OutputFile { get; set; }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(opts => RunOptionsAndReturnExitCode(opts))
+                .WithNotParsed((errs) => HandleParseError(errs));
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errs)
+        {
+            return;
+        }
+
+        private static void RunOptionsAndReturnExitCode(Options opts)
+        {
+           
         }
     }
 }
