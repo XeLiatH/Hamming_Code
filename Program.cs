8a6754a85b34c422d0234d6062291bd0f20807c8
Matěj Beran <matej.beran96@gmail.com>
2019-12-05 20:38:19 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hamming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
