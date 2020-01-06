addea93de17482f67fcec1e5e475a4429ab4aff6
Matěj Beran <matej.beran96@gmail.com>
2020-01-06 17:28:47 +0100

    Remove reference comment.

--- Hamming/Crc32.cs
+++ Hamming/Crc32.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 namespace Hamming
 {
-    /*
-
-        Reference
-        - http://sanity-free.org/12/crc32_implementation_in_csharp.html
-        - https://stackoverflow.com/questions/2587766/how-is-a-crc32-checksum-calculated
-        - https://rosettacode.org/wiki/CRC-32#C.23
-        - https://en.wikipedia.org/wiki/Cyclic_redundancy_check
-
-    */
-
     public class Crc32
     {
         private const uint POLYNOMIAL = 0xEDB88320; // note: g(x) generator polynomial set by IEEE 802.3
